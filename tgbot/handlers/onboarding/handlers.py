1c4656e10be4f8335bc7b4cd2eeeb310b6959c6a
Good-for-noth1ng <apasnost@list.ru>
2022-04-22 17:18:22 +0200

    changin start

--- tgbot/handlers/onboarding/handlers.py
+++ tgbot/handlers/onboarding/handlers.py
@@ -12,15 +12,14 @@ from tgbot.handlers.utils.info import extract_user_data_from_update
 #creating new user
 from tgbot.models import User
 
-def command_start(update: Update, context: CallbackContext) -> None:
-    u, created = User.get_user_and_created(update, context)
+def command_start(update: Update, context: CallbackContext):
+    # u, created = User.get_user_and_created(update, context)
     update.message.reply_text("start pressed")
-    if created:
-        text = static_text.start_created.format(first_name=u.first_name)
-    else:
-        text = static_text.start_not_created.format(first_name=u.first_name)
-
-    update.message.reply_text(text=text, reply_markup=make_keiboard_for_start())
+    # if created:
+    #     text = static_text.start_created.format(first_name=u.first_name)
+    # else:
+    #     text = static_text.start_not_created.format(first_name=u.first_name)
+    # update.message.reply_text(text=text, reply_markup=make_keiboard_for_start())
 
 def make_keiboard_for_start() -> InlineKeyboardMarkup:
     buttons = [[
